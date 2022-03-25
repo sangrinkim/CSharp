8635349e2b77af87ffb7d0bcdee0ed41f056fc46
Superb <staret.vision@gmail.com>
2022-03-25 23:52:53 +0900

    Test C# 11
    - Generic Attribute (preview version)

---
+++ cs11/cs11test/cs11test/GenericAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs11test
+{
+    public class TypeAttribute : Attribute
+    {
+        public TypeAttribute(Type t) => ParamType = t;
+
+        public Type ParamType { get; }
+    }
+
+    // You must set <LangVersion>preview<LangVersion> in project file
+    public class GenericAttribute<T> : Attribute { }
+
+    public class Test
+    {
+        // C# 11 feature
+        [TypeAttribute(typeof(string))]
+        public string TestOldFeature() => default;
+
+        [GenericAttribute<string>()]
+        public string TestNewFeature() => default;
+
+        // Wrong example;
+        //[GenericAttribute<T>()] // Generic attributes must be fully constructed types.
+        //public string NotAllowedMethod() => default;
+    }
+}
